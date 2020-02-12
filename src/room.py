897b202d62ec1983223a7a6708d42439a8b5a619
Victor <arowovici@gmail.com>
2020-02-12 17:25:17 +0100

    Fleshed out room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,20 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def __init__(self, name, description, items=[]):
+        self.name = name
+        self.description = description
+        self.items = items
+
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def __getitem__(self, arg):
+        return getattr(self, arg)
